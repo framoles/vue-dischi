4be7b7fec242b840987003115d7c5c667e8fa570
Francesco Moles <francesco.moles@outlook.it>
2022-04-11 18:07:12 +0200

    add filter

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,11 +1,22 @@
 <template>
   <main>
-    <div v-for="(item, index) in array" :key="index">
-      <img :src="item.poster" :alt="item.title" />
-      <p>{{ item.title }}</p>
-      <p>{{ item.author }}</p>
-      <p>{{ item.year }}</p>
-    </div>
+    <header>
+      <select @change="displaySelected($event)">
+        <option value="Genere">Genere</option>
+        <option value="Pop">Pop</option>
+        <option value="Metal">Metal</option>
+        <option value="Jazz">Jazz</option>
+        <option value="Rock">Rock</option>
+      </select>
+    </header>
+    <footer>
+      <div v-for="(item, index) in filtered" :key="index">
+        <img :src="item.poster" :alt="item.title" />
+        <p>{{ item.title }}</p>
+        <p>{{ item.author }}</p>
+        <p>{{ item.year }}</p>
+      </div>
+    </footer>
   </main>
 </template>
 
@@ -18,6 +29,8 @@ export default {
   data() {
     return {
       array: Array,
+      genere: "Genere",
+      filtered: Array,
     };
   },
   mounted() {
@@ -25,18 +38,42 @@ export default {
       .get("https://flynn.boolean.careers/exercises/api/array/music")
       .then((resp) => {
         this.array = resp.data.response;
+        this.filtered = this.array;
       });
+  },
+  methods: {
+    displaySelected(event) {
+      this.genere = event.target.value;
+
+      if (this.genere == "Genere") {
+        this.filtered = this.array;
+      } else {
+        const result = this.array.filter((item) => {
+          return item.genre.includes(this.genere);
+        });
+
+        this.filtered = result;
+      }
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-main {
+header {
+  select {
+    margin-bottom: 20px;
+    font-size: 20px;
+    border: 2px dotted red;
+  }
+}
+footer {
   display: flex;
   column-gap: 30px;
+  background-color: rgb(53, 67, 145);
   div {
-    width: 100px;
-    background-color: rgb(37, 35, 56);
+    width: calc((100% / 5) - 30px);
+    background-color: rgb(86, 110, 243);
     padding: 10px;
     color: white;
     img {
